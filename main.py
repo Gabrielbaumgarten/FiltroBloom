cf08e24735bc5b9670cf23c055c50beb05ee433e
Henrique da Gama Saczkowski <henrique1443@hotmail.com>
2022-04-12 15:11:51 -0300

    Up

--- main.py
+++ main.py
@@ -18,20 +18,34 @@ def main ():
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
     cv2.imshow("imagem Padrão", img)
-    imgHLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS) #Transforma a immagem em HLS
+
+    #Transforma a immagem em HLS
+    imgHLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS) 
     #cv2.imshow("imagemhsl", imgHLS)  
+
+    #Bright-pass  
     Lchannel = imgHLS[:,:,1]    
-    mask = cv2.inRange(Lchannel, 125, 255) #Bright-pass    
-    res = cv2.bitwise_and(img,img, mask= mask) #Cria a máscara
+    mask = cv2.inRange(Lchannel, 125, 255)  
+
+    #Cria a máscara
+    res = cv2.bitwise_and(img,img, mask= mask) 
     #cv2.imshow("Máscara", res)
-    blur = cv2.GaussianBlur(res,(0,0),10) #Blur gaussiano
-    '''blur=cv2.blur(res, (19,19)) #Blur com filtro da média
+
+    #Blur gaussiano
+    blur = cv2.GaussianBlur(res,(0,0),10)
+
+    #Blur com filtro da média
+    '''blur=cv2.blur(res, (19,19)) 
     for i in range (2): #Quantidade de vezes para realizar filtro da média
         blur=cv2.blur(blur, (19,19))'''
+
     #cv2.imshow("Imagem borrada", blur)
-    end = cv2.addWeighted(img, 1, blur, 0.5, 0) #Imagem final juntando máscara e imagem
+
+    #Imagem final juntando máscara e imagem
+    end = cv2.addWeighted(img, 1, blur, 0.5, 0) 
     cv2.imshow("Imagem Final", end)
     #cv2.imwrite('final.png', end)
+
     cv2.waitKey ()
     cv2.destroyAllWindows ()
 
